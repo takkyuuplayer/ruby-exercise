1f68d6fc4d2c202dc013ecc70661aad15425a393
takkyuuplayer <takkyuuplayer@gmail.com>
2017-11-16 16:08:10 -0500

    Add module

---
+++ test/mixin_test.rb
@@ -0,0 +1,23 @@
+require "minitest/autorun"
+
+class StringNumber
+  include Comparable
+  def <=>(rhs)
+    @number.to_i <=> rhs.number.to_i
+  end
+
+  def initialize(number)
+    @number = number.to_s
+  end
+  attr_accessor :number
+end
+
+class MixinTest < Minitest::Test
+  def test_mixin
+    one = StringNumber.new(1)
+    two = StringNumber.new(2)
+    ten = StringNumber.new(10)
+
+    assert_equal [one, two, ten], [two, one, ten].sort
+  end
+end
---
+++ test/namespace_test.rb
@@ -0,0 +1,47 @@
+require "minitest/autorun"
+
+module Space1
+  class StringNumber
+    include Comparable
+    def <=>(rhs)
+      @number.to_s <=> rhs.number.to_s
+    end
+
+    def initialize(number)
+      @number = number.to_s
+    end
+    attr_accessor :number
+  end
+end
+
+module Space2
+  class StringNumber
+    include Comparable
+    def <=>(rhs)
+      @number.to_i <=> rhs.number.to_i
+    end
+
+    def initialize(number)
+      @number = number.to_s
+    end
+    attr_accessor :number
+  end
+end
+
+class NamespaceTest < Minitest::Test
+  def test_string_comparison
+    one = Space1::StringNumber.new(1)
+    two = Space1::StringNumber.new(2)
+    ten = Space1::StringNumber.new(10)
+
+    assert_equal [one, ten, two], [two, one, ten].sort
+  end
+
+  def test_number_comparison
+    one = Space2::StringNumber.new(1)
+    two = Space2::StringNumber.new(2)
+    ten = Space2::StringNumber.new(10)
+
+    assert_equal [one, two, ten], [two, one, ten].sort
+  end
+end
