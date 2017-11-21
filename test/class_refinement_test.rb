c9495e0688acb34e9899e3ff6a30140b07fb3f8a
takkyuuplayer <takkyuuplayer@gmail.com>
2017-11-21 18:10:04 -0500

    Add dup/clone, refinment test

---
+++ test/class_refinement_test.rb
@@ -0,0 +1,40 @@
+require "minitest/autorun"
+
+class Klass
+  attr_accessor :num
+
+  def initialize(num)
+    @num = num
+  end
+
+  def calc
+    @num * @num
+  end
+
+end
+
+module M
+  refine Klass do
+    def calc
+      @num * @num * @num
+    end
+  end
+end
+
+class RefinmentTestNotRefined < Minitest::Test
+  def test_refinement
+    k = Klass.new(2)
+
+    assert_equal 4, k.calc
+  end
+end
+
+using M
+
+class RefinmentTestRefined < Minitest::Test
+  def test_refinement
+    k = Klass.new(2)
+
+    assert_equal 8, k.calc
+  end
+end
---
+++ test/dup_clone_test.rb
@@ -0,0 +1,45 @@
+require "minitest/autorun"
+
+class Klass
+  attr_accessor :num
+  def initialize(num)
+    @num = num
+  end
+end
+
+class DupCloneTest < Minitest::Test
+  def test_clone_will_copy_singular_method_or_frozen_state
+    k = Klass.new(2)
+
+    def k.square
+      @num * @num
+    end
+    k.freeze
+
+    assert_equal 4, k.square
+    assert_equal true, k.frozen?
+
+    cloned = k.clone
+
+    assert_equal true, cloned.methods.include?(:square)
+    assert_equal true, cloned.frozen?
+  end
+
+  def test_dup_will_not_copy_singular_method_or_frozen_state
+    k = Klass.new(2)
+
+    def k.square
+      @num * @num
+    end
+    k.taint
+    k.freeze
+
+    assert_equal true, k.methods.include?(:square)
+    assert_equal true, k.frozen?
+
+    duped = k.dup
+
+    assert_equal false, duped.methods.include?(:square)
+    assert_equal false, duped.frozen?
+  end
+end
